ee16cd24f19953698469e3077a820a4c29b3980d
Joel-Ax <joelaxelsson97@gmail.com>
2024-11-06 12:56:54 +0100

    Hej

---
+++ src/test/java/com/example/libraryofalexandria/LibraryOfAlexandriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryofalexandria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryOfAlexandriaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
