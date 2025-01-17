f953c060f8e93abd8f550fa6e87c7ccd6da34f3c
djaevil <djaevil1@gmail.com>
2025-01-17 09:10:46 +0100

    Removed unwanted code on DTOs

--- src/main/java/com/example/libraryofalexandria/DTO/AdminDTO.java
+++ src/main/java/com/example/libraryofalexandria/DTO/AdminDTO.java
@@ -1,20 +1,14 @@
 package com.example.libraryofalexandria.DTO;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class AdminDTO {
-
-    @Id
     private Long id;
-
     private String username;
     private String role;
 }
--- src/main/java/com/example/libraryofalexandria/DTO/UserDTO.java
+++ src/main/java/com/example/libraryofalexandria/DTO/UserDTO.java
@@ -1,20 +1,14 @@
 package com.example.libraryofalexandria.DTO;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class UserDTO {
-
-    @Id
     private Long id;
-    
     private String firstName;
     private String lastName;
     private String email;
